fd1ad519320f4ca02972533091b14eee6873b488
iozervos <iozervos@ubuntu.(none)>
2013-11-13 13:01:46 +0100

    there is a bug

--- Assignment1.py
+++ Assignment1.py
@@ -1,8 +1,11 @@
+from __future__ import division
 import glob, re, nltk, string, pickle
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from collections import defaultdict
+
+
 
 #process each document in the directory 'collection'
 doclist = glob.glob("collection/*.txt")
@@ -14,6 +17,7 @@ strlenEndString = len(endstring)
 stemmer=PorterStemmer()
 InvertedIndex = {}
 tot_tokens = []
+docidx = []
 
 for docname in doclist:
     if docname.startswith(startstring):
@@ -31,20 +35,28 @@ for docname in doclist:
     file_content = re.sub('[%s]' % re.escape(string.punctuation), '', file_content)
     #tokenization
     file_content = nltk.word_tokenize(file_content)
-    print "tokens before preprocessing: "+str(len(file_content))
+    #print "tokens before preprocessing: "+str(len(file_content))
     tot_tokens.append(len(file_content))
     #stemming + count term frequency
+
+    #self.file = [w for w in self.file if w not in stopwords.words('english')]
     d = defaultdict(int)
     stemmed_Words = []
     for word in file_content:
-        stemmedWord = stemmer.stem(word)
-        stemmed_Words.append(stemmedWord)
-        d[stemmedWord] += 1
+        if word not in stopwords.words('english'): 
+            stemmedWord = stemmer.stem(word)
+            stemmedWord = word        
+            stemmed_Words.append(stemmedWord)
+            d[stemmedWord] += 1
         
     #build the inverted index
     for word in stemmed_Words:
         locations = InvertedIndex.setdefault(word, {})
         locations[docname] = d[word]
+        #d[word] += 1#float(d[stemmedWord]/len(stemmed_Words))
+        d[word] = float(d[word]/len(stemmed_Words))
+        print word, d[word]
+
 
 print "total tokens: "+str(sum(tot_tokens))
 print "unique terms: "+str(len(InvertedIndex)) 
@@ -58,17 +70,21 @@ print "unique terms: "+str(len(InvertedIndex))
 #    print i
 
 pickle.dump(InvertedIndex, open("SavedInvertedIndex.p", "wb"))
+
+
 loaded_data = pickle.load(open( "SavedInvertedIndex.p", "rb" ))
 #for i in loaded_data.items():
 #    print i
 
-idxof = loaded_data['of'].items()
+#idxof = loaded_data['of'].items()
 cnt = 0
-for i in idxof:
+#for i in idxof:
     #print i[1]
-    cnt = cnt + i[1]
+    #cnt = cnt + i[1]
 
-print "Of appears: " + str(cnt)
+#print "Of appears: " + str(cnt)
 
+#for i in loaded_data:
+    #print i+" "+str(loaded_data[i])+" "+str(len(loaded_data[i]))
 
 
