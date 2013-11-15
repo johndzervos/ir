0cc300b832e366d9cac98b374504004031308422
liveris <liveris1@yahoo.com>
2013-11-15 01:35:21 +0100

    The query works for more than two terms. we have to make it work for one term only

--- query.py
+++ query.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import division
 import glob, re, nltk, string, pickle
 from nltk.stem import PorterStemmer
@@ -6,6 +7,8 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from collections import defaultdict
 from utilFunctions import *
 import math,operator
+from itertools import groupby, chain
+from operator import itemgetter
 
 loaded_data = pickle.load(open( "SavedInvertedIndex.p", "rb" ))
 stemmer=PorterStemmer()
@@ -14,7 +17,8 @@ stemmer=PorterStemmer()
 
 def preprocessQuery(query):
     tot_tokens = []
-    file_content = query.lower()
+    file_content = query.read()
+    file_content = file_content.lower()
     #remove odd chars
     file_content = re.sub(r'[^a-z0-9 ]',' ',file_content)
     #remove punctuation
@@ -32,29 +36,63 @@ def preprocessQuery(query):
             stemmed_Words.append(stemmedWord)
     return stemmed_Words
 
+def getListOfDocsOfTerm(processedQuery):
+    #check if the query word exists
+    try:
+        idx = loaded_data[processedQuery].items()
+    except:
+        idx=-1
+    return idx
 
-#-----------------It works only for one term in the query, need to be exetended----------
-
+#-----------------It works only for 2 and more terms in the query, better doublecheck----------
 #example query 
-query='june8'
+query = open("sampleQuery.txt")
 #process query
 processedQuery=preprocessQuery(query)
-#empty dictionary of the ranked docs
+print processedQuery
+#dictionary of the final ranked docs
 rankedListOfDocs={}
-#check if the query word exists
-try:
-    idx = loaded_data[processedQuery[0]].items()
-except:
-    idx=-1
-if(idx!=-1):
-    #take the freq of the doc which is the second place of the list in a list of lists
-    for sublist in idx:
-        noDocs=262 #toatal number of docs in collection, maybe we need to obtain it dynamically
-        #compute tfidf, 
-        rankedListOfDocs[sublist[0]]=(1+math.log(sublist[1]))*math.log(noDocs/len(loaded_data[processedQuery[0]].items()))
+#dict of the intersection of returned docs
+final={}
+#we need the counter so as to start from the seond element in the below loop
+#cause in the first loop there is only one term, no intersection
+count=1
+
+#for every term in the query
+for term in processedQuery:
+    #get the docs of each term
+    index=getListOfDocsOfTerm(term)
+    #check if there is the term in the doc
+    if(index!=-1& count!=1):
+        #convert the list to dict to process quicker and easier
+        dict_a = dict(index)
+        dict_b=dict(combined)
+        #common docs between terms
+        all_keys = set(dict_b.keys()) | set(dict_a.keys())
+        #make a new dict of the intersection of the docs
+        final = dict((k, (dict_a.get(k), dict_b.get(k))) for k in all_keys)
+    combined=index
+    count=count+1
+
+#for every doc in dict 
+for key in final:
+    #pointer to get the corresponding idf from the posting l
+    counter=0
+    sumScore=0
+    for v in final[key]:
+         #some terms occur only in one doc,the other returns none
+         if (v!=None):
+            noDocs=262 #total number of docs in collection, maybe we need to obtain it dynamically
+            #compute tf
+            tf=(1+float(math.log(float(v))))
+            idf=math.log(noDocs/float(len(loaded_data[processedQuery[counter]].items())))
+            score=tf*idf
+            sumScore=sumScore+score
+            counter=counter+1
+    #assign to the rank dict the score of both terms
+    rankedListOfDocs[key]=sumScore
 #sort the dictionary based on the value returned by score tfidf
-    sortedRankOfDocs = sorted(rankedListOfDocs.iteritems(), key=operator.itemgetter(1))
-    print sortedRankOfDocs
-else:
-    print "Query term not in collection!"
+sortedRankOfDocs = sorted(rankedListOfDocs.iteritems(), key=operator.itemgetter(1))
+print sortedRankOfDocs
+
     
